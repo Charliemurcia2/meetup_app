05b8956a5ad4042f5e6ced691433e2f120d19152
Carlos Murcia <carlos.murciam@perficient.com>
2022-10-23 19:56:22 -0500

    full app with reducers and context

--- src/hooks/use-request.js
+++ src/hooks/use-request.js
@@ -1,52 +1,54 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const useRequest = () => {
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState('')
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const bodySetter = (body, method) => {
-    switch(method) {
-      case 'POST':
-        return ({
-          method,
-            body: JSON.stringify(body),
-            headers: {
-              'Content-Type': 'application/json'
-            }
-        })
-      case 'DELETE':
-        return ({
-          method
-        })
-      case 'PATCH':
-        return ({
+    switch (method) {
+      case "POST":
+        return {
           method,
           body: JSON.stringify(body),
           headers: {
-            'Content-Type': 'application/json'
-          }
-        })
-      default :
-        return null
+            "Content-Type": "application/json",
+          },
+        };
+      case "DELETE":
+        return {
+          method,
+        };
+      case "PATCH":
+        return {
+          method,
+          body: JSON.stringify(body),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+      default:
+        return null;
     }
-  }
-  
-  const callAPI = async (url, method = 'GET', body = null) => {
-    setIsLoading('true')
+  };
+
+  const callAPI = async (url, method = "GET", body = null) => {
+    console.log("in");
+    setIsLoading("true");
     const response = await fetch(url, bodySetter(body, method));
-    setIsLoading(false)
+    console.log(response);
+    setIsLoading(false);
     if (!response.ok) {
-      setError('something is wrong')
-      throw Error(`something is wrong. ${response.status}`)
+      setError("something is wrong");
+      throw Error(`something is wrong. ${response.status}`);
     }
-    return response.json()
-  }
+    return response.json();
+  };
 
   return {
     isLoading,
     error,
-    callAPI
-  }
-}
+    callAPI,
+  };
+};
 
-export default useRequest
+export default useRequest;
